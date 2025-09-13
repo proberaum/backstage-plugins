9b85d455361566b40d63a3aebe560354dc398323
Christoph Jerolimov <jerolimov+git@gmail.com>
2025-09-13 07:46:53 +0200

    chore(config-viewer): create new backend plugin
    Signed-off-by: Christoph Jerolimov <jerolimov+git@gmail.com>

--- workspaces/config-viewer/packages/backend/src/index.ts
+++ workspaces/config-viewer/packages/backend/src/index.ts
@@ -63,4 +63,7 @@ backend.add(import('@backstage/plugin-kubernetes-backend'));
 backend.add(import('@backstage/plugin-notifications-backend'));
 backend.add(import('@backstage/plugin-signals-backend'));
 
+// config-viewer plugin
+backend.add(import('@proberaum/backstage-plugin-config-viewer-backend'));
+
 backend.start();
---
+++ workspaces/config-viewer/plugins/config-viewer-backend/src/index.ts
@@ -0,0 +1,1 @@
+export { configViewerPlugin as default } from './plugin';
